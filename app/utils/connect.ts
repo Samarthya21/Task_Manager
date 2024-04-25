b76b1dfabe820556a3e795723a12efd1a85a5a72
Samarthya21 <112236357+Samarthya21@users.noreply.github.com>
2024-04-25 23:06:34 +0530

    Update connect.ts

--- app/utils/connect.ts
+++ app/utils/connect.ts
@@ -1,20 +1,25 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
-declare const global: {
-  prisma?: any; // Adjust the type according to your PrismaClient type
-};
 
-if(process.env.NODE_ENV === 'production'){
-    prisma= new PrismaClient();
-}
-else{
-    if(!global.prisma){
-        global.prisma= new PrismaClient();
+// Check if the environment is production
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient();
+} else {
+    // Declare global and its property prisma
+    declare global {
+        namespace NodeJS {
+            interface Global {
+                prisma?: PrismaClient;
+            }
+        }
     }
-    else{
-        prisma= global.prisma;
+
+    // Check if prisma has already been initialized globally
+    if (!global.prisma) {
+        global.prisma = new PrismaClient();
     }
+    prisma = global.prisma;
 }
 
 export default prisma;
